bae3d1fe5ee4847483aad4f8a59769b00b0f578a
deadman96385 <seanhoyt963@gmail.com>
2015-04-24 01:29:35 -0400

    Liquify

--- liquid.mk
+++ liquid.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/samsung/klimtwifi/full_klimtwifi.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_klimtwifi
+PRODUCT_NAME := liquid_klimtwifi
 PRODUCT_DEVICE := klimtwifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
