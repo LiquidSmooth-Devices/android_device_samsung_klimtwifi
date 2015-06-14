92d30068bb644de307497f8adee4ba4cc9eb68d6
Sean Hoyt <seanhoyt963@gmail.com>
2015-06-14 01:15:44 -0500

    Use new tablet config

--- liquid.mk
+++ liquid.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/klimtwifi/full_klimtwifi.mk)
 
 # Inherit some common Liquid stuff.
-$(call inherit-product, vendor/liquid/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/liquid/config/common_tablet_wifionly.mk)
 
 PRODUCT_NAME := liquid_klimtwifi
 PRODUCT_DEVICE := klimtwifi
